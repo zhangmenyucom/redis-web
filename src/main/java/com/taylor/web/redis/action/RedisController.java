b07a844c63f9b7461df08f670d74008e818c0361
xiaolu.zhang <xiaolu.zhang@weimob.com>
2016-12-21 14:09:42 +0800

    更新

--- src/main/java/com/taylor/web/redis/action/RedisController.java
+++ src/main/java/com/taylor/web/redis/action/RedisController.java
@@ -1,5 +1,7 @@
 package com.taylor.web.redis.action;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taylor.redis.service.RedisClientService;
 
@@ -25,6 +28,12 @@ public class RedisController {
 
 	@Autowired
 	private RedisClientService redisClientService;
+
+	public static AtomicInteger success_count = new AtomicInteger(0);
+
+	public static volatile int fail_count = 0;
+
+	public static int version = 0;
 
 	/**
 	 * 
@@ -72,4 +81,56 @@ public class RedisController {
 		}
 		return "redis/index";
 	}
+
+	/**
+	 * @desc distributeLockTest(这里用一句话描述这个方法的作用)
+	 * @param request
+	 * @param response
+	 * @return
+	 * @author xiaolu.zhang
+	 * @date 2016年12月1日 上午11:06:27
+	 */
+	@RequestMapping("/test")
+	@ResponseBody
+	public String distributeLockTest(@RequestParam("key") String key) {
+		/** 设置锁值为当前时间戳 **/
+		Long value = System.currentTimeMillis();
+		/** getset命令：设置新值并返回原来的值，如果原来key不存在则返回null **/
+		String oldValue = redisClientService.getSet(key, value + "");
+		/** 不存在则拥有锁 **/
+		if (null == oldValue) {
+			/** 设置有效时间为1秒 **/
+			redisClientService.expire(key, 1);
+
+			/** 处理业务逻辑 **/
+			{
+				System.out.println("doBusiness......");
+			}
+			/** 处理完业务逻辑，释放锁 **/
+			redisClientService.del(key);
+			success_count.incrementAndGet();
+			System.out.println("成功次数：" + success_count.get());
+			return "sucess";
+		} else {
+			Long timeGap = System.currentTimeMillis() - Long.parseLong(oldValue);
+			/** 业务超时,释放锁 **/
+			if (timeGap > 500) {
+				redisClientService.del(key);
+			}
+			fail_count++;
+			System.out.println("重试次数：" + fail_count);
+			 return "fail";
+			//return this.distributeLockTest(key);
+		}
+
+	}
+	
+/*	@RequestMapping("/queue")
+	@ResponseBody
+	public String redusQueueTest(String message) {
+		
+		redisClientService.lpush("list", "message");
+		redisClientService.blpop(message)
+		
+	}*/
 }
